e2f80d87bd7b2ae19618f0776ed344cc916beb5f
bowb <6180319+bowb@users.noreply.github.com>
2020-04-21 18:57:56 -0400

    initial commit

---
+++ autoresponder.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+import getpass, poplib, sys, smtplib, ssl, getopt 
+from email.parser import Parser
+from email.header import decode_header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+inbox = ''
+outbox = ''
+popServer = ''
+popPort = ''
+popSSL = False
+smtpServer = ''
+popPassword = ''
+smtpPassword = ''
+smtpSSL = False
+smtpPort = ''
+verbose = False
+
+poplib._MAXLINE = 2147483647 
+words = ['priesthood','covenant','blessing','brethren','sister','stake','ward','elder','church','lord','sacrament','ministering','saints','jesus']
+
+def main():
+    global inbox, outbox, popServer, popPort, popSSL, smtpServer, popPassword, smtpPassword, smtpSSL, smtpPort, verbose
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"vhi:o:p:s:",["inbox=","outbox=","pop=","smtp=","pop-password=","smtp-password=", "pop-port=","smtp-port=","smtpSSL","popSSL"])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-i","--inbox"):
+            inbox = arg
+        elif opt in ("-o","--outbox"):
+            outbox = arg
+        elif opt in ("-p","--pop"):
+            popServer = arg
+        elif opt in ("-s","--smtp"):
+            smtpServer = arg
+        elif opt == "--pop-password":
+            popPassword = arg
+        elif opt == "--smtp-password":
+            smtpPassword = arg
+        elif opt == "--pop-port":
+            popPort = arg
+        elif opt == "--smtp-port":
+            smtpPort = arg
+        elif opt == "smtpSSL":
+            smtpSSL = True
+        elif opt == "popSSL":
+            popSSL = True
+        elif opt == "-v":
+            verbose = True
+        else:
+            assert False, "unhandled option"
+
+    hasrequired = True
+    if(inbox == ''):
+        hasrequired = False
+        print("inbox is required")
+    if(outbox == ''):
+        hasrequired = False
+        print("outbox is required")
+    if(popServer == ''):
+        hasrequired = False
+        print("popserver is required")
+    if(popPort == ""):
+        hasrequired = False
+        print("pop-port is required")
+    if(smtpServer == ""):
+        hasrequired = False
+        print ("smtp-server is required")
+    if(popPassword == ""):
+        hasrequired = False
+        print("pop-password is required")
+    if(smtpPassword == ""):
+        hasrequired = False
+        print("smtp-password is required")
+    if(smtpPort == ""):
+        hasrequired = False
+        print("smtp-port is required")
+
+    if(not hasrequired):
+        usage()
+        sys.exit(2)
+
+    if(verbose):
+        print("POP_USER:" + inbox)
+        print("POP_PASSWORD:" + popPassword)
+        print("POP_SERVER:" + popServer)
+        print("POP_PORT:" + popPort);
+        print("SMTP_USER:" + outbox)
+        print("SMTP_PASSWORD:" + smtpPassword)
+        print("SMTP_PORT:" + smtpPort)
+
+
+    Mailbox = poplib.POP3_SSL(popServer, popPort)
+    Mailbox.user(inbox) 
+    Mailbox.pass_(popPassword) 
+    numMessages = len(Mailbox.list()[1])
+
+    for i in range(numMessages):
+        resp, lines, octets = Mailbox.retr(i+1) 
+        msg_content = b'\r\n'.join(lines).decode('utf-8')
+        for w in words:
+            if w in msg_content.lower():
+                msg = Parser().parsestr(msg_content)
+                message = Get_info(msg)
+                subject = msg.get('Subject')
+                date = msg.get('Date')
+                replyTo = msg.get('Reply-to')
+                sender = msg.get('From')
+                if(replyTo):
+                    sendResponse(replyTo, subject) 
+                elif(sender):
+                    sendResponse(sender, subject)
+                break
+
+    Mailbox.quit()
+
+def guess_charset(msg):
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos >= 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
+
+
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decode(charset)
+    return value
+
+
+def Get_info(msg):
+    if (msg.is_multipart()):
+        parts = msg.get_payload()
+        for n, part in enumerate(parts):
+            return Get_info(part)
+    if not msg.is_multipart():
+        content_type = msg.get_content_type()
+        if content_type == 'text/plain':
+            content = msg.get_payload(decode=True)
+            charset = guess_charset(msg)
+            if charset:
+                content = content.decode(charset)
+            return content
+
+
+def sendResponse(to, subject):
+    global inbox, outbox, popServer, popPort, popSSL, smtpServer, popPassword, smtpPassword, smtpSSL, smtpPort
+    try:
+        sender = outbox 
+        context = ssl.create_default_context()
+        
+        message = MIMEMultipart('alternative')
+        message['Subject'] = subject
+        message['From'] = sender
+        message['To'] = to
+        txt = """\
+        https://www.lds.org/topics/plural-marriage-in-kirtland-and-nauvoo?lang=eng
+
+        The footnotes gave me a bit of a shock actually. It said that Joseph married between 30-40 women, 12-14 were already married to other living men, that he was intimate with his wives (one as young as 14) and the he possibly had 2-3 children with them.
+            
+        That can't be right can it?
+        """
+        txtPart = MIMEText(txt,'plain')
+        html = """\
+        <a href='https://www.lds.org/topics/plural-marriage-in-kirtland-and-nauvoo?lang=eng'>Learn about Helen Mar Kimball</a>
+        <br/>
+        The footnotes gave me a bit of a shock actually. It said that Joseph married between 30-40 women, 12-14 were already married to other living men, that he was intimate with his wives (one as young as 14) and the he possibly had 2-3 children with them.
+        <br />   
+        <br />
+        <b><i>That can't be right can it?</i></b>
+        """
+        htmlPart = MIMEText('<div dir="auto">' + html + '</div>','html')
+        message.attach(txtPart)
+        message.attach(htmlPart)
+        with smtplib.SMTP_SSL(smtpServer,smtpPort,context=context) as server:
+            server.login(sender,smtpPassword)
+            server.sendmail(sender, to, message.as_string())
+    except Exception as e:
+        print(e)
+
+
+def usage():
+    print(sys.argv[0] + " -i <inbox> -o <outbox> -p <pop3server> -s <smtpserver> --pop-port=<port> --smtp-port=<port> --pop-password=<passwprd> --smtp-password=<password>")
+
+
+if __name__ == "__main__":
+    main()
+
